65fa558c28dd2afe338fb44e4f15e2b5799f7942
vanpham <ptvan0311@gmail.com>
2023-07-25 14:11:40 +0700

    fix: test mans projectid

--- currents.config.js
+++ currents.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  projectId: "Ij0RfK",
+  projectId: "2zy7tg",
+  recordKey: "Uu907UaLX1KmesKl",
+  // cloudServiceUrl: "https://cy.currents.dev", // Sorry Cypress users - the director service URL
+  e2e: {
+    batchSize: 3, // orchestration batch size for e2e tests (Currents only, read below)
+  },
+  component: {
+    batchSize: 5, // orchestration batch size for component tests (Currents only, read below)
+  },
 };
